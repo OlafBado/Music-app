d7533e59fc419fa2a43b205a357abccca6a10c9a
OlafWindrunner <niebiel1@gmail.com>
2022-09-16 10:29:52 +0200

    a

---
+++ reactapp/cypress/e2e/spec.cy.ts
@@ -0,0 +1,16 @@
+import cypress from "cypress"
+
+describe('empty spec', { scrollBehavior: false }, () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+    cy.findByPlaceholderText(/search albums/i).type('rock', { force: true })
+    cy.findByRole('row', { name: /the mars volta the mars volta rock/i }).children('.td-svg').click()
+    cy.findByRole('button', {  name: /favourites/i}).click()
+    cy.findAllByRole('row').should('have.length', 2)
+    cy.findByRole('row', { name: /the mars volta the mars volta rock/i }).children('.td-svg').click()
+    cy.findAllByRole('row').should('have.length', 0)
+    cy.findByRole('button', {  name: /favourites/i}).click()
+    cy.findByPlaceholderText(/search albums/i).clear()
+    cy.findAllByRole('row').should('have.length.greaterThan', 2)
+  })
+})
